4de921bffbbf8045119215c4f2ee939a57364ad0
Pablo <Pablo20101997@hotmail.es>
2020-01-23 20:27:48 +0100

    Creación del esqueleto d BigNumber y BigNumberTest

--- src/BigNumber.java
+++ src/BigNumber.java
@@ -1,13 +1,15 @@
 class BigNumber {
     double b;
-    // Constructor 1 -- Lo utilizaremos para mostrar todas las operaciones.
+
+    // Constructor 1
     public BigNumber(String s) {
-
+    BigNumber n1 = new BigNumber("4");
     }
-    // Constructor 2 -- Lo utilizaremos para
+    // Constructor 2
     public BigNumber(BigNumber b) {
-
+    BigNumber n2 = new BigNumber("3");
     }
+
     // Suma
     BigNumber add(BigNumber other) {
 
@@ -44,7 +46,14 @@ class BigNumber {
     // Compara dos BigNumber. Torna 0 si són iguals, -1
 // si el primer és menor i torna 1 si el segon és menor
     public int compareTo(BigNumber other) {
-        return 0;
+        if (this == other) {
+            return 0;
+        } else if (this.b < other.b) {
+            return -1;
+        } else if (this.b > other.b) {
+            return 1;
+        }
+        return -5;
     }
     // Torna un String representant el número
     public String toString() {
@@ -52,6 +61,11 @@ class BigNumber {
     }
     // Mira si dos objectes BigNumber són iguals
     public boolean equals(Object other) {
+        if(this == other) {
+            return true;
+        }else if(!(other instanceof BigNumber)){ // SI other no es un BigNumber dará error
+            return false;
+        }
         return false;
     }
 }
